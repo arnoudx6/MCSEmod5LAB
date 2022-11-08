9546d76a75a935bbe76b84cea2a0f0d965278d62
arnoudx6 <arnoud.stolk@ziggo.nl>
2022-11-08 15:01:19 +0100

    Add files via upload

---
+++ unquotedServicePaths.ps1
@@ -0,0 +1,6 @@
+﻿#Retrieve all services
+$services = Get-CimInstance win32_service | select State, Name, DisplayName, PathName, StartName
+
+#Find unquoted service paths
+#Paths that do not contain " quotes and do contain space chars before the .exe
+$services | where{($_.PathName -notlike '"*"*') -and ($_.PathName -like '* *.exe*')} | FT
